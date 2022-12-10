de7f3e55a7ad7b4bb79d49e0c9b8f14de35c4414
Reinhard Schu <reinhard@schu.name>
2022-12-10 22:49:08 +0000

    Update basic_routines.asm

--- include/basic_routines.asm
+++ include/basic_routines.asm
@@ -5,15 +5,15 @@
 ; Last Updated:	10/12/2022
 ;
 
-; Print a zero-terminated string
+; Print a zero-terminated string pointed to by HL
 ;
-PRSTR:	LD		A,(HL)
-		OR		A
-		RET		Z
+PRSTR:	LD	A,(HL)
+		OR	A
+		RET	Z
 #ifdef ADL
 		RST.LIL	$10
 #else
-    	RST		$10
+    		RST	$10
 #endif
-		INC		HL
-		JR		PRSTR
+		INC	HL
+		JR	PRSTR
