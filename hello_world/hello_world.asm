7580fc5be6c3c8b7411cdfa3a98915d8b0234ed3
Reinhard Schu <reinhard@schu.name>
2022-12-11 16:18:33 +0000

    add print hex numbers to helper functions

--- hello_world/hello_world.asm
+++ hello_world/hello_world.asm
@@ -2,10 +2,10 @@
 ; Title:	Hello World - Main
 ; Author:	Dean Belfield, Reinhard Schu
 ; Created:	06/11/2022
-; Last Updated:	09/12/2022
+; Last Updated:	11/12/2022
 
 ; define or undefine ADL (24-bit) mode
-	#define ADL
+	#undefine ADL
 ;	#undefine ADL
 
 #include "../include/init.asm"
@@ -16,10 +16,22 @@
 MAIN:	        
                 LD	HL, HELLO_WORLD
 		CALL	PRSTR
-		LD	HL, 0
+                LD      HL,$07
+;
+;       Test printing Hex numbers
+;
+		LD	A,$A7
+                CALL    Print_Hex8
+                PRT_CRLF
+                LD      HL,$E39F
+                CALL    Print_Hex16
+                PRT_CRLF
+                LD.L    HL,$6A9BF4
+                CALL    Print_Hex24
+                PRT_CRLF
+                LD      HL,0                    ;return zero to MOS
 		RET
 	
 ; Sample text
 ;
 HELLO_WORLD:	.DB	"Hello World\n\r",0
-
--- include/helper_functions.asm
+++ include/helper_functions.asm
@@ -1,19 +1,67 @@
 ;
-; Title:	Basic Routines
+; Title:	Helper Functions
 ; Author:	Dean Belfield, Reinhard Schu
 ; Created:	06/11/2022
-; Last Updated:	10/12/2022
+; Last Updated:	11/12/2022
 ;
 
 ; Print a zero-terminated string pointed to by HL
 ;
-PRSTR:		LD	A,(HL)
-		OR	A
-		RET	Z
+#MACRO  PRT_CHR
 #ifdef ADL
 		RST.LIL	$10
 #else
 		RST	$10
 #endif
+#ENDMACRO
+
+#MACRO  PRT_CRLF
+                LD      A,'\n'
+                PRT_CHR
+                LD      A,'\r'
+                PRT_CHR
+#ENDMACRO
+
+
+PRSTR:		LD	A,(HL)
+		OR	A
+		RET	Z
+                PRT_CHR
 		INC	HL
 		JR	PRSTR
+; Print a zero-terminated string
+;
+; Print a 24-bit HEX number
+; HLU: Number to print
+;
+Print_Hex24:	PUSH.LIL	HL
+		LD.LIL		HL, 2
+		ADD.LIL		HL, SP
+		LD.LIL		A, (HL)
+		POP.LIL		HL
+		CALL		Print_Hex8			
+			
+; Print a 16-bit HEX number
+; HL: Number to print
+;
+Print_Hex16:	LD	A,H
+		CALL	Print_Hex8
+		LD	A,L
+
+; Print an 8-bit HEX number
+; A: Number to print
+;
+Print_Hex8:	LD	C,A
+		RRA 
+		RRA 
+		RRA 
+		RRA 
+		CALL	prtnbl 
+		LD	A,C 
+prtnbl:		AND	0Fh
+		ADD	A,90h
+		DAA
+		ADC	A,40h
+		DAA
+		PRT_CHR
+		RET
