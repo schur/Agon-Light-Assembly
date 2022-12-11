8f5927c59ffaef2754b9a1c583576c5c48b13088
Reinhard Schu <reinhard@schu.name>
2022-12-11 13:47:21 +0000

    fix tabs

--- include/init.asm
+++ include/init.asm
@@ -5,33 +5,32 @@
 ; Last Updated:	09/12/2022
 ;
 #ifdef ADL
-			; Start in ADL mode
-			.ASSUME	ADL = 1
-			; Set Code location to $40000 (MOS default user RAM)
-			.ORG $40000
+		; Start in ADL mode
+		.ASSUME	ADL = 1
+		; Set Code location to $40000 (MOS default user RAM)
+		.ORG $40000
 #else
-			.ASSUME	ADL = 0
-			; Set Code location to $0000 (as we are in 16-bit mode)
-			.ORG $0000
+		.ASSUME	ADL = 0
+		; Set Code location to $0000 (as we are in 16-bit mode)
+		.ORG $0000
 #endif
-
-			JR		_START		; Jump to start
-			.BLOCK	6
+		JR		_START		; Jump to start
+		.BLOCK	6
 
 #ifdef ADL
-			; if in ADL (24-bit) mode, fill; with 0 up to byte $40 (MOS header)
-			.BLOCK $38
+		; if in ADL (24-bit) mode, fill; with 0 up to byte $40 (MOS header)
+		.BLOCK $38
 #else
-			; set RST vectors if in Z80 compatibility (16-bit) mode
-			; which will call the "real" RST vector
+		; set RST vectors if in Z80 compatibility (16-bit) mode
+		; which will call the "real" RST vector
 
-RST_08:		RST.LIS		08h		; API call
-			RET
-			.BLOCK	5
+RST_08:		RST.LIS	08h		; API call
+		RET
+		.BLOCK	5
 			
-RST_10:		RST.LIS 	10h		; Output
-			RET
-			.BLOCK	5
+RST_10:		RST.LIS	10h		; Output
+		RET
+		.BLOCK	5
 			
 RST_18:		.BLOCK	8
 RST_20:		.BLOCK	8
@@ -41,26 +40,26 @@ RST_30:		.BLOCK	8
 ; The NMI interrupt vector (not currently used by AGON)
 ;
 RST_38:		EI
-			RETI
-			.BLOCK  5
+		RETI
+		.BLOCK  5
 #endif
 
 ; The header stuff is from byte $40 onwards
 
-            .DB		"MOS"	; Flag for MOS - to confirm this is a valid MOS command
-			.DB		0		; MOS header version 0
+                .DB	"MOS"	; Flag for MOS - to confirm this is a valid MOS command
+		.DB	0		; MOS header version 0
 #ifdef ADL
-			.DB		1		; Flag for run mode (0: Z80, 1: ADL)
+		.DB	1		; Flag for run mode (0: Z80, 1: ADL)
 #else
-			.DB		0
+		.DB	0
 #endif
 ;
 ; And the code follows on immediately after the header
 ;
 _START:		EI				; Enable the MOS interrupts
-			CALL	MAIN	; Start user Code
+		CALL	MAIN	; Start user Code
 #ifdef ADL
-			RET
+		RET
 #else
-			RET.L
+		RET.L
 #endif
